f13b90487256eea2df03a01fa786e353d5a04fdd
Zoe Martin <zoemartin71@gmail.com>
2019-09-02 16:30:18 -0400

    Added "Hello Dolly from Eclipse Again"

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,7 +3,8 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello Dolly in eclipse");
+		System.out.println("Hello Dolly from Eclipse Again");
 
 	}
 
