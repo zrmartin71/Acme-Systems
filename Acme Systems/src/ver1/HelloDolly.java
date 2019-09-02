b2e1b75a168dc7932bf49ef81c1a0fd85bbc9023
Zoe Martin <zoemartin71@gmail.com>
2019-09-02 16:41:00 -0400

    Added more text to HelloDolly

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,7 +3,7 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Dolly in eclipse");
+		System.out.println("Hello Dolly from eclipse");
 		System.out.println("Hello Dolly from Eclipse Again");
 
 	}
